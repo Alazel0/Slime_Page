a818b7020f93108c07be9902e869288363f6da25
Alazel <98067486+Alazel0@users.noreply.github.com>
2025-03-04 15:15:54 -0300

    Update link_button.py

--- Slime_Page/components/link_button.py
+++ Slime_Page/components/link_button.py
@@ -2,7 +2,14 @@ import reflex as rx
 
 def link_button(text: str, url: str) -> rx.Component:
     return rx.link(
-        rx.button(text),
+        rx.button(
+            rx.hstack(
+                rx.icon(
+                    tag= "album"
+                ),
+                rx.text(text)
+            )
+        ),
         href= url,
         is_external= True,
         width = "100%"
