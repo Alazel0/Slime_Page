a97dfa0f90ffc5031f3b7e6edebc550c09bbcc08
Alazel <98067486+Alazel0@users.noreply.github.com>
2025-03-07 22:36:41 -0300

    avíspese

--- Slime_Page/Slime_Page.py
+++ Slime_Page/Slime_Page.py
@@ -2,6 +2,9 @@ import reflex as rx
 import Slime_Page.styles.styles as styles
 from Slime_Page.pages.index import index
 
+class State(rx.State):
+    """definir un estado"""
+
 app = rx.App(
     style = styles.BASE_STYLE
 )
