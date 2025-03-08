7fb7916cb9edb2f964f72d0919b26f1d54d480aa
Alazel <98067486+Alazel0@users.noreply.github.com>
2025-03-08 00:44:16 -0300

    kuka

--- remote_build.sh
+++ remote_build.sh
@@ -1,9 +1,11 @@
+cd Slime_Page
+python -m venv .venv
 source .venv/Scripts/activate
 pip install --upgrade pip
 pip install -r requirements.txt
+rm -rf public
 reflex init
 reflex export --frontend-only
-rm -rf public
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
