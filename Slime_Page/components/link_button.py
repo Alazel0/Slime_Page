8aadffb398124a43580ac727602504143385a923
Alazel <98067486+Alazel0@users.noreply.github.com>
2025-03-08 15:21:26 -0300

    oso

--- Slime_Page/components/link_button.py
+++ Slime_Page/components/link_button.py
@@ -16,10 +16,3 @@ def link_button(text: str, url: str) -> rx.Component:
         is_external=True,
         width="100%"
     )
-
-def login_page() -> rx.Component:
-    return rx.vstack(
-        link_button("Login with Discord", "/auth/discord"),
-        link_button("Login with Twitter", "/auth/twitter"),
-        spacing="1rem",
-    )
--- Slime_Page/views/links.py
+++ Slime_Page/views/links.py
@@ -1,12 +1,11 @@
 import reflex as rx
 from Slime_Page.components.link_button import link_button
-from Slime_Page.components.title import title
 
 def links() -> rx.Component:
     return rx.vstack(
         rx.hstack(
-            link_button("Twitter", "http:/www.ggg.com"),
+            link_button("Login with Discord", "/auth/discord"),
             rx.spacer(),
-            link_button("Discord", "http:/www.ggg.com")
+            link_button("Login with Twitter", "/auth/twitter")
         )
     )
